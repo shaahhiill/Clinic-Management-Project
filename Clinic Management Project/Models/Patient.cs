7c52f0017739f64492013adcb5d07cbec5b2d99c
Shaahil Seedin <shaahilseedin@gmail.com>
2025-06-05 13:19:28 +0530

    Register patient info nad program class added

--- Clinic Management Project/Models/Patient.cs
+++ Clinic Management Project/Models/Patient.cs
@@ -1,14 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Clinic_Management_Project.Models
 {
-    internal class Patient
+    class Patient
     {
-        my name is arzaq
+        // Encapsulated properties
+        public string FirstName;
+        public string LastName;
+        public DateTime DateOfBirth;
+        public string Gender;
+        public string ContactNumber;
+        public string MedicalHistory;
+
+        // Method to register patient into the database
+        public void RegisterPatient()
+        {
+            string connectionString = "Server=localhost;Database=ClinicDB;Trusted_Connection=True;";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "INSERT INTO Patients (FirstName, LastName, DateOfBirth, Gender, ContactNumber, MedicalHistory) " +
+                                   "VALUES (@FirstName, @LastName, @DOB, @Gender, @Contact, @History)";
+
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@FirstName", FirstName);
+                        cmd.Parameters.AddWithValue("@LastName", LastName);
+                        cmd.Parameters.AddWithValue("@DOB", DateOfBirth);
+                        cmd.Parameters.AddWithValue("@Gender", Gender);
+                        cmd.Parameters.AddWithValue("@Contact", ContactNumber);
+                        cmd.Parameters.AddWithValue("@History", MedicalHistory);
+
+                        cmd.ExecuteNonQuery();
+                        Console.WriteLine("Patient registered successfully!");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+            }
+        }
     }
-    hihihi
+
 }
+
--- Clinic Management Project/Program.cs
+++ Clinic Management Project/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Clinic_Management_Project.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,29 @@ namespace Clinic_Management_Project
     {
         static void Main(string[] args)
         {
+            Patient p = new Patient();
+
+            // Get patient details
+            Console.Write("Enter First Name: ");
+            p.FirstName = Console.ReadLine();
+
+            Console.Write("Enter Last Name: ");
+            p.LastName = Console.ReadLine();
+
+            Console.Write("Enter Date of Birth (yyyy-mm-dd): ");
+            p.DateOfBirth = DateTime.Parse(Console.ReadLine());
+
+            Console.Write("Enter Gender: ");
+            p.Gender = Console.ReadLine();
+
+            Console.Write("Enter Contact Number: ");
+            p.ContactNumber = Console.ReadLine();
+
+            Console.Write("Enter Medical History: ");
+            p.MedicalHistory = Console.ReadLine();
+
+            // Register the patient
+            p.RegisterPatient();
         }
     }
 }
