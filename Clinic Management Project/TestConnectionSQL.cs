a571619433d27fbf49c0bb1f619335bdf809b2cd
zaqyy <arzaqauffer07@gmail.com>
2025-06-13 12:32:53 +0530

    testing SQL

---
+++ Clinic Management Project/TestConnectionSQL.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using MySql.Data.MySqlClient; // Ensure you have MySql.Data package installed
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinic_Management_Project
+{
+    class TestConnectionSQL
+    {
+        public static void TestConnection()
+        {
+            using (MySqlConnection conn = new MySqlConnection(DatabaseConfig.ConnectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    Console.WriteLine("✅ Connection successful!");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("❌ Connection failed: " + ex.Message);
+                }
+            }
+        }
+    }
+}
