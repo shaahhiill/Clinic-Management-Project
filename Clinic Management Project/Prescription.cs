a663d27aa5c4306e3c00f24e64dab4585a1a5bd4
Muhammed Akram <muhammedakram6618@gmail.com>
2025-06-13 13:48:58 +0530

    Create prescription and added related code

---
+++ Clinic Management Project/Prescription.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinic_Management_Project
+{
+    internal class Prescription
+    {
+        public string PrescriptionID;
+        public string PatientID;
+        public string DoctorID;
+        public string Medication;
+        public string Dosage;
+        public DateTime DateIssued;
+
+        public Prescription(string prescriptionID, string patientID, string doctorID, string medication, string dosage, DateTime dateIssued)
+        {
+            PrescriptionID = prescriptionID;
+            PatientID = patientID;
+            DoctorID = doctorID;
+            Medication = medication;
+            Dosage = dosage;
+            DateIssued = dateIssued;
+        }
+    }
+}
