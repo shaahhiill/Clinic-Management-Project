7e101636a4f8b5161be169897d0693e442ce9cb4
zaqyy <arzaqauffer07@gmail.com>
2025-06-13 20:36:27 +0530

    updated user.cs

--- Clinic Management Project/Models/Appointment.cs
+++ Clinic Management Project/Models/Appointment.cs
@@ -16,44 +16,6 @@ namespace Clinic_Management_Project.Models
         public int DoctorID;
         public bool NotificationSent;
 
-        // create
-        public void Book(string connStr)
-        {
-            using (SqlConnection conn = new SqlConnection(connStr))
-            {
-                conn.Open();
-                SqlCommand cmd = new SqlCommand("INSERT INTO Appointments (DateTime, Status, PatientName, DoctorID, NotificationSent) VALUES (@dt, 'Booked', @p, @d, 0)", conn);
-                cmd.Parameters.AddWithValue("@dt", DateTime);
-                cmd.Parameters.AddWithValue("@p", PatientName);
-                cmd.Parameters.AddWithValue("@d", DoctorID);
-                cmd.ExecuteNonQuery();
-            }
-        }
-
-        // delete
-
-        public void Cancel(string connStr)
-        {
-            using (SqlConnection conn = new SqlConnection(connStr))
-            {
-                conn.Open();
-                SqlCommand cmd = new SqlCommand("UPDATE Appointments SET Status='Canceled' WHERE AppointmentID=@id", conn);
-                cmd.Parameters.AddWithValue("@id", AppointmentID);
-                cmd.ExecuteNonQuery();
-            }
-        }
-
-        // update
-        public void Reschedule(string connStr, DateTime newTime)
-        {
-            using (SqlConnection conn = new SqlConnection(connStr))
-            {
-                conn.Open();
-                SqlCommand cmd = new SqlCommand("UPDATE Appointments SET DateTime=@dt WHERE AppointmentID=@id", conn);
-                cmd.Parameters.AddWithValue("@dt", newTime);
-                cmd.Parameters.AddWithValue("@id", AppointmentID);
-                cmd.ExecuteNonQuery();
-            }
-        }
+        
     }
 }
