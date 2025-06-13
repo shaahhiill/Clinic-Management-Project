55625603efba97dcebf7a852305c795624833326
Muhammed Akram <muhammedakram6618@gmail.com>
2025-06-13 12:05:51 +0530

    Created presecription variable

--- Clinic Management Project/Models/Prescription.cs
+++ Clinic Management Project/Models/Prescription.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Runtime.Remoting.Channels;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,6 +11,21 @@ namespace Clinic_Management_Project.Models
 {
     internal class Prescription
     {
+        public string PrescriptionID;
+        public string PatientID;
+        public string DoctorID;
+        public string Medication;
+        public string Diagnosis;
+        public DateTime DateIssued;
 
+        public Prescription(string prescriptionID, string PatientID, string DoctorID, string Medication, string Diagnosis, DateTime DateIssued)
+        {
+            PrescriptionID = prescriptionId;
+            PatientID = patientId;
+            DoctorID = doctorId;
+            Medication = medication;
+            Dosage = dosage;
+            DateIssued = DateTime.Now;
+        }
     }
 }
