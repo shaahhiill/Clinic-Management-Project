4c0bd6b79aeef1e81f05097f8f6975be9f622619
Shaahil Seedin <shaahilseedin@gmail.com>
2025-06-14 13:43:29 +0530

    Made the GUI for the MainForm

---
+++ Clinic Management Project/GUI/MainForm.cs
@@ -0,0 +1,58 @@
+﻿using Clinic_Management_Project.UI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clinic_Management_Project.GUI
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var form = new FormPatient();
+            form.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var form = new FormDoctor();
+            form.ShowDialog();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var form = new FormAppointment();
+            form.ShowDialog();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var form = new FormPrescription();
+            form.ShowDialog();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            var form = new FormReports();
+            form.ShowDialog();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            // Log out / close main form
+            this.Close();
+        }
+    }
+}
