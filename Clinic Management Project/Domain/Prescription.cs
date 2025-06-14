736fc7c332d2603670823e110a3ee5a1df717c34
Muhammedakram-2006 <muhammedakram6618@gmail.com>
2025-06-14 21:29:27 +0530

    Arangeed the class ansd windows form to the related file

--- Clinic Management Project/Domain/Prescription.cs
+++ Clinic Management Project/Domain/Prescription.cs
@@ -4,9 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Clinic_Management_Project.Domain
+namespace Clinic_Management_Project
 {
     internal class Prescription
     {
+        public string PrescriptionID;
+        public string PatientID;
+        public string DoctorID;
+        public string Medication;
+        public string Dosage;
+
+        public Prescription(string prescriptionID, string patientID, string doctorID, string medication, string dosage)
+        {
+            PrescriptionID = prescriptionID;
+            PatientID = patientID;
+            DoctorID = doctorID;
+            Medication = medication;
+            Dosage = dosage;
+        }
+
     }
 }
--- Clinic Management Project/Services/PrescriptionManager.cs
+++ Clinic Management Project/Services/PrescriptionManager.cs
@@ -4,9 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Clinic_Management_Project.Services
+namespace Clinic_Management_Project
 {
     internal class PrescriptionManager
     {
+        private List<Prescription> prescriptions = new List<Prescription>();
+        public void AddPrescription(Prescription p)
+        {
+            prescriptions.Add(p);
+        }
+
+        public List<Prescription> GetAll()
+        {
+            return prescriptions;
+        }
+
+        public Prescription GetByID(string id)
+        {
+            return prescriptions.FirstOrDefault(p => p.PrescriptionID == id);
+        }
+
+        public bool EditPrescription(string id, string med, string dosage)
+        {
+            var p = GetByID(id);
+            if (p != null)
+            {
+                p.Medication = med;
+                p.Dosage = dosage;
+                return true;
+            }
+            return false;
+        }
+
+
     }
 }
