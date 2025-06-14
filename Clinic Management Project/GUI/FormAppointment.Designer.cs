18179a64119dd812ecc63c1d3fc9a8bad208a21f
zaqyy <arzaqauffer07@gmail.com>
2025-06-15 02:34:53 +0530

    coded and designed appointment form

--- Clinic Management Project/GUI/FormAppointment.Designer.cs
+++ Clinic Management Project/GUI/FormAppointment.Designer.cs
@@ -2,15 +2,19 @@
 {
     partial class FormAppointment
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Label lblPatient;
+        private System.Windows.Forms.Label lblDoctor;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.ComboBox cmbPatient;
+        private System.Windows.Forms.ComboBox cmbDoctor;
+        private System.Windows.Forms.DateTimePicker dtpAppointment;
+        private System.Windows.Forms.Button btnBook;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.DataGridView dgvAppointments;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,159 +24,73 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.LblSelectDoctor = new System.Windows.Forms.Label();
-            this.LblApptDateTime = new System.Windows.Forms.Label();
-            this.CboDoctors = new System.Windows.Forms.ComboBox();
-            this.DtpApptDateTime = new System.Windows.Forms.DateTimePicker();
-            this.BtnBookAppt = new System.Windows.Forms.Button();
-            this.dgvAppointments = new System.Windows.Forms.GroupBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.BtnCancelAppt = new System.Windows.Forms.Button();
-            this.BtnRescheduleAppt = new System.Windows.Forms.Button();
-            this.groupBox1.SuspendLayout();
-            this.dgvAppointments.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.lblPatient = new System.Windows.Forms.Label();
+            this.lblDoctor = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.cmbPatient = new System.Windows.Forms.ComboBox();
+            this.cmbDoctor = new System.Windows.Forms.ComboBox();
+            this.dtpAppointment = new System.Windows.Forms.DateTimePicker();
+            this.btnBook = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.dgvAppointments = new System.Windows.Forms.DataGridView();
+
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAppointments)).BeginInit();
             this.SuspendLayout();
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.BtnBookAppt);
-            this.groupBox1.Controls.Add(this.DtpApptDateTime);
-            this.groupBox1.Controls.Add(this.CboDoctors);
-            this.groupBox1.Controls.Add(this.LblApptDateTime);
-            this.groupBox1.Controls.Add(this.LblSelectDoctor);
-            this.groupBox1.Location = new System.Drawing.Point(12, 22);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(534, 281);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "New Appointment";
-            // 
-            // LblSelectDoctor
-            // 
-            this.LblSelectDoctor.AutoSize = true;
-            this.LblSelectDoctor.Location = new System.Drawing.Point(18, 37);
-            this.LblSelectDoctor.Name = "LblSelectDoctor";
-            this.LblSelectDoctor.Size = new System.Drawing.Size(57, 20);
-            this.LblSelectDoctor.TabIndex = 0;
-            this.LblSelectDoctor.Text = "Doctor";
-            this.LblSelectDoctor.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // LblApptDateTime
-            // 
-            this.LblApptDateTime.AutoSize = true;
-            this.LblApptDateTime.Location = new System.Drawing.Point(18, 87);
-            this.LblApptDateTime.Name = "LblApptDateTime";
-            this.LblApptDateTime.Size = new System.Drawing.Size(86, 20);
-            this.LblApptDateTime.TabIndex = 1;
-            this.LblApptDateTime.Text = "Date & Time";
-            this.LblApptDateTime.Click += new System.EventHandler(this.lblApptDateTime_Click);
-            // 
-            // CboDoctors
-            // 
-            this.CboDoctors.FormattingEnabled = true;
-            this.CboDoctors.Location = new System.Drawing.Point(125, 37);
-            this.CboDoctors.Name = "CboDoctors";
-            this.CboDoctors.Size = new System.Drawing.Size(276, 28);
-            this.CboDoctors.TabIndex = 2;
-            this.CboDoctors.SelectedIndexChanged += new System.EventHandler(this.cboDoctors_SelectedIndexChanged);
-            // 
-            // DtpApptDateTime
-            // 
-            this.DtpApptDateTime.Location = new System.Drawing.Point(125, 87);
-            this.DtpApptDateTime.Name = "DtpApptDateTime";
-            this.DtpApptDateTime.Size = new System.Drawing.Size(276, 26);
-            this.DtpApptDateTime.TabIndex = 3;
-            this.DtpApptDateTime.ValueChanged += new System.EventHandler(this.dtpApptDateTime_ValueChanged);
-            // 
-            // BtnBookAppt
-            // 
-            this.BtnBookAppt.Location = new System.Drawing.Point(251, 135);
-            this.BtnBookAppt.Name = "BtnBookAppt";
-            this.BtnBookAppt.Size = new System.Drawing.Size(150, 29);
-            this.BtnBookAppt.TabIndex = 4;
-            this.BtnBookAppt.Text = "Book Appointment";
-            this.BtnBookAppt.UseVisualStyleBackColor = true;
-            this.BtnBookAppt.Click += new System.EventHandler(this.btnBookAppt_Click);
-            // 
-            // dgvAppointments
-            // 
-            this.dgvAppointments.Controls.Add(this.BtnRescheduleAppt);
-            this.dgvAppointments.Controls.Add(this.BtnCancelAppt);
-            this.dgvAppointments.Controls.Add(this.dataGridView1);
-            this.dgvAppointments.Location = new System.Drawing.Point(13, 378);
-            this.dgvAppointments.Name = "dgvAppointments";
-            this.dgvAppointments.Size = new System.Drawing.Size(533, 313);
-            this.dgvAppointments.TabIndex = 1;
-            this.dgvAppointments.TabStop = false;
-            this.dgvAppointments.Text = "My Appointments";
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(21, 37);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 62;
-            this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(489, 220);
-            this.dataGridView1.TabIndex = 0;
-            // 
-            // BtnCancelAppt
-            // 
-            this.BtnCancelAppt.Location = new System.Drawing.Point(21, 263);
-            this.BtnCancelAppt.Name = "BtnCancelAppt";
-            this.BtnCancelAppt.Size = new System.Drawing.Size(98, 30);
-            this.BtnCancelAppt.TabIndex = 1;
-            this.BtnCancelAppt.Text = "Cancel";
-            this.BtnCancelAppt.UseVisualStyleBackColor = true;
-            this.BtnCancelAppt.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // BtnRescheduleAppt
-            // 
-            this.BtnRescheduleAppt.Location = new System.Drawing.Point(407, 263);
-            this.BtnRescheduleAppt.Name = "BtnRescheduleAppt";
-            this.BtnRescheduleAppt.Size = new System.Drawing.Size(103, 30);
-            this.BtnRescheduleAppt.TabIndex = 2;
-            this.BtnRescheduleAppt.Text = "Reschedule";
-            this.BtnRescheduleAppt.UseVisualStyleBackColor = true;
-            // 
-            // FormAppointment
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1272, 843);
+
+            this.lblPatient.Text = "Patient";
+            this.lblPatient.Location = new System.Drawing.Point(30, 30);
+            this.cmbPatient.Location = new System.Drawing.Point(120, 30);
+            this.cmbPatient.Size = new System.Drawing.Size(200, 23);
+
+            this.lblDoctor.Text = "Doctor";
+            this.lblDoctor.Location = new System.Drawing.Point(30, 70);
+            this.cmbDoctor.Location = new System.Drawing.Point(120, 70);
+            this.cmbDoctor.Size = new System.Drawing.Size(200, 23);
+
+            this.lblDate.Text = "Date/Time";
+            this.lblDate.Location = new System.Drawing.Point(30, 110);
+            this.dtpAppointment.Location = new System.Drawing.Point(120, 110);
+            this.dtpAppointment.Size = new System.Drawing.Size(200, 23);
+
+            this.btnBook.Text = "Book";
+            this.btnBook.Location = new System.Drawing.Point(30, 150);
+            this.btnBook.Click += new System.EventHandler(this.btnBook_Click);
+
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.Location = new System.Drawing.Point(110, 150);
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.Location = new System.Drawing.Point(190, 150);
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+
+            this.btnClear.Text = "Clear";
+            this.btnClear.Location = new System.Drawing.Point(270, 150);
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+
+            this.dgvAppointments.Location = new System.Drawing.Point(30, 190);
+            this.dgvAppointments.Size = new System.Drawing.Size(500, 200);
+
+            this.ClientSize = new System.Drawing.Size(600, 420);
+            this.Controls.Add(this.lblPatient);
+            this.Controls.Add(this.cmbPatient);
+            this.Controls.Add(this.lblDoctor);
+            this.Controls.Add(this.cmbDoctor);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.dtpAppointment);
+            this.Controls.Add(this.btnBook);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnClear);
             this.Controls.Add(this.dgvAppointments);
-            this.Controls.Add(this.groupBox1);
-            this.Name = "FormAppointment";
-            this.Text = "FormAppointment";
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
-            this.dgvAppointments.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Text = "Appointment Management";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAppointments)).EndInit();
             this.ResumeLayout(false);
-
+            this.PerformLayout();
         }
-
-        #endregion
-
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.DateTimePicker DtpApptDateTime;
-        private System.Windows.Forms.ComboBox CboDoctors;
-        private System.Windows.Forms.Label LblApptDateTime;
-        private System.Windows.Forms.Label LblSelectDoctor;
-        private System.Windows.Forms.Button BtnBookAppt;
-        private System.Windows.Forms.GroupBox dgvAppointments;
-        private System.Windows.Forms.Button BtnRescheduleAppt;
-        private System.Windows.Forms.Button BtnCancelAppt;
-        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- Clinic Management Project/GUI/FormAppointment.cs
+++ Clinic Management Project/GUI/FormAppointment.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ClinicManagementSystem;
+using MySql.Data.MySqlClient;
 
 namespace Clinic_Management_Project.GUI
 {
@@ -15,6 +17,177 @@ namespace Clinic_Management_Project.GUI
         public FormAppointment()
         {
             InitializeComponent();
+            LoadAppointments();
+            LoadPatients();
+            LoadDoctors();
+        }
+
+        private void LoadAppointments()
+        {
+            MySqlConnection conn = DatabaseConnection.GetConnection();
+            try
+            {
+                conn.Open();
+                string sql = "SELECT a.id, p.name AS patient, d.name AS doctor, a.appointment_date, a.status " +
+                             "FROM appointments a " +
+                             "JOIN patients p ON a.patient_id = p.id " +
+                             "JOIN doctors d ON a.doctor_id = d.id";
+                MySqlDataAdapter adapter = new MySqlDataAdapter(sql, conn);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                dgvAppointments.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void LoadPatients()
+        {
+            MySqlConnection conn = DatabaseConnection.GetConnection();
+            try
+            {
+                conn.Open();
+                string sql = "SELECT id, name FROM patients";
+                MySqlDataAdapter adapter = new MySqlDataAdapter(sql, conn);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                cmbPatient.DataSource = dt;
+                cmbPatient.DisplayMember = "name";
+                cmbPatient.ValueMember = "id";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void LoadDoctors()
+        {
+            MySqlConnection conn = DatabaseConnection.GetConnection();
+            try
+            {
+                conn.Open();
+                string sql = "SELECT id, name FROM doctors";
+                MySqlDataAdapter adapter = new MySqlDataAdapter(sql, conn);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                cmbDoctor.DataSource = dt;
+                cmbDoctor.DisplayMember = "name";
+                cmbDoctor.ValueMember = "id";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void btnBook_Click(object sender, EventArgs e)
+        {
+            MySqlConnection conn = DatabaseConnection.GetConnection();
+            try
+            {
+                conn.Open();
+                string sql = "INSERT INTO appointments (patient_id, doctor_id, appointment_date, status) VALUES (@patient_id, @doctor_id, @date, 'Scheduled')";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@patient_id", cmbPatient.SelectedValue);
+                cmd.Parameters.AddWithValue("@doctor_id", cmbDoctor.SelectedValue);
+                cmd.Parameters.AddWithValue("@date", dtpAppointment.Value);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Appointment booked successfully!");
+                LoadAppointments();
+                ClearFields();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (dgvAppointments.CurrentRow == null) return;
+
+            string id = dgvAppointments.CurrentRow.Cells[0].Value.ToString();
+            MySqlConnection conn = DatabaseConnection.GetConnection();
+            try
+            {
+                conn.Open();
+                string sql = "UPDATE appointments SET patient_id=@patient_id, doctor_id=@doctor_id, appointment_date=@date WHERE id=@id";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.Parameters.AddWithValue("@patient_id", cmbPatient.SelectedValue);
+                cmd.Parameters.AddWithValue("@doctor_id", cmbDoctor.SelectedValue);
+                cmd.Parameters.AddWithValue("@date", dtpAppointment.Value);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Appointment updated successfully!");
+                LoadAppointments();
+                ClearFields();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            if (dgvAppointments.CurrentRow == null) return;
+
+            string id = dgvAppointments.CurrentRow.Cells[0].Value.ToString();
+            MySqlConnection conn = DatabaseConnection.GetConnection();
+            try
+            {
+                conn.Open();
+                string sql = "UPDATE appointments SET status='Cancelled' WHERE id=@id";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Appointment cancelled successfully!");
+                LoadAppointments();
+                ClearFields();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearFields();
+        }
+
+        private void ClearFields()
+        {
+            cmbPatient.SelectedIndex = -1;
+            cmbDoctor.SelectedIndex = -1;
+            dtpAppointment.Value = DateTime.Now;
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -45,6 +218,11 @@ namespace Clinic_Management_Project.GUI
         private void button1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //nothing here
         }
     }
 }
