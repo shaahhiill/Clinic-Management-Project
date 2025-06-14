42159cf2954e97806d6b521859206b901efbac15
zaqyy <arzaqauffer07@gmail.com>
2025-06-15 01:34:25 +0530

    coded and designed FormPatient

--- Clinic Management Project/GUI/FormPatient.Designer.cs
+++ Clinic Management Project/GUI/FormPatient.Designer.cs
@@ -2,15 +2,14 @@
 {
     partial class FormPatient
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Label lblName, lblDOB, lblGender, lblAddress, lblPhone;
+        private System.Windows.Forms.TextBox txtName, txtAddress, txtPhone;
+        private System.Windows.Forms.DateTimePicker dtpDOB;
+        private System.Windows.Forms.ComboBox cmbGender;
+        private System.Windows.Forms.Button btnSave, btnNew, btnEdit, btnDelete, btnClear;
+        private System.Windows.Forms.DataGridView dgvPatients;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,249 +19,125 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblDOB = new System.Windows.Forms.Label();
+            this.lblGender = new System.Windows.Forms.Label();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.lblPhone = new System.Windows.Forms.Label();
             this.txtName = new System.Windows.Forms.TextBox();
+            this.dtpDOB = new System.Windows.Forms.DateTimePicker();
+            this.cmbGender = new System.Windows.Forms.ComboBox();
             this.txtAddress = new System.Windows.Forms.TextBox();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
             this.btnSave = new System.Windows.Forms.Button();
-            this.BtnClear = new System.Windows.Forms.Button();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.BtnNewPatient = new System.Windows.Forms.Button();
-            this.BtnEditPatient = new System.Windows.Forms.Button();
-            this.BtnDelete = new System.Windows.Forms.Button();
-            this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.dgvPatients = new System.Windows.Forms.DataGridView();
+
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPatients)).BeginInit();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(84, 62);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Name";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(84, 122);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(99, 20);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Date of Birth";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(84, 186);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(63, 20);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Gender";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(84, 240);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(68, 20);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "Address";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(84, 295);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(55, 20);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "Phone";
-            // 
+
+            // lblName
+            this.lblName.Text = "Name";
+            this.lblName.Location = new System.Drawing.Point(30, 30);
+            this.lblName.Size = new System.Drawing.Size(100, 23);
+
             // txtName
-            // 
-            this.txtName.Location = new System.Drawing.Point(264, 62);
-            this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(223, 26);
-            this.txtName.TabIndex = 5;
-            this.txtName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            // 
+            this.txtName.Location = new System.Drawing.Point(140, 30);
+            this.txtName.Size = new System.Drawing.Size(200, 23);
+
+            // lblDOB
+            this.lblDOB.Text = "Date of Birth";
+            this.lblDOB.Location = new System.Drawing.Point(30, 70);
+            this.lblDOB.Size = new System.Drawing.Size(100, 23);
+
+            // dtpDOB
+            this.dtpDOB.Location = new System.Drawing.Point(140, 70);
+            this.dtpDOB.Size = new System.Drawing.Size(200, 23);
+
+            // lblGender
+            this.lblGender.Text = "Gender";
+            this.lblGender.Location = new System.Drawing.Point(30, 110);
+            this.lblGender.Size = new System.Drawing.Size(100, 23);
+
+            // cmbGender
+            this.cmbGender.Location = new System.Drawing.Point(140, 110);
+            this.cmbGender.Size = new System.Drawing.Size(200, 23);
+            this.cmbGender.Items.AddRange(new object[] { "Male", "Female", "Other" });
+
+            // lblAddress
+            this.lblAddress.Text = "Address";
+            this.lblAddress.Location = new System.Drawing.Point(30, 150);
+            this.lblAddress.Size = new System.Drawing.Size(100, 23);
+
             // txtAddress
-            // 
-            this.txtAddress.Location = new System.Drawing.Point(264, 237);
-            this.txtAddress.Name = "txtAddress";
-            this.txtAddress.Size = new System.Drawing.Size(223, 26);
-            this.txtAddress.TabIndex = 6;
-            this.txtAddress.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
-            // 
-            // dateTimePicker1
-            // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(264, 122);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(221, 26);
-            this.dateTimePicker1.TabIndex = 8;
-            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(264, 178);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(223, 28);
-            this.comboBox1.TabIndex = 9;
-            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
-            // 
+            this.txtAddress.Location = new System.Drawing.Point(140, 150);
+            this.txtAddress.Size = new System.Drawing.Size(200, 23);
+
+            // lblPhone
+            this.lblPhone.Text = "Phone";
+            this.lblPhone.Location = new System.Drawing.Point(30, 190);
+            this.lblPhone.Size = new System.Drawing.Size(100, 23);
+
+            // txtPhone
+            this.txtPhone.Location = new System.Drawing.Point(140, 190);
+            this.txtPhone.Size = new System.Drawing.Size(200, 23);
+
             // btnSave
-            // 
-            this.btnSave.Location = new System.Drawing.Point(264, 295);
-            this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(88, 36);
-            this.btnSave.TabIndex = 10;
             this.btnSave.Text = "Save";
-            this.btnSave.UseVisualStyleBackColor = true;
-            this.btnSave.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // BtnClear
-            // 
-            this.BtnClear.Location = new System.Drawing.Point(380, 295);
-            this.BtnClear.Name = "BtnClear";
-            this.BtnClear.Size = new System.Drawing.Size(96, 36);
-            this.BtnClear.TabIndex = 11;
-            this.BtnClear.Text = "Clear";
-            this.BtnClear.UseVisualStyleBackColor = true;
-            this.BtnClear.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.BtnDelete);
-            this.groupBox1.Controls.Add(this.BtnEditPatient);
-            this.groupBox1.Controls.Add(this.BtnNewPatient);
-            this.groupBox1.Controls.Add(this.dataGridView1);
-            this.groupBox1.Location = new System.Drawing.Point(88, 423);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(695, 222);
-            this.groupBox1.TabIndex = 12;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Patient List";
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Column1,
-            this.Column2});
-            this.dataGridView1.Location = new System.Drawing.Point(24, 41);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 62;
-            this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(481, 62);
-            this.dataGridView1.TabIndex = 0;
-            // 
-            // Column1
-            // 
-            this.Column1.HeaderText = "Column1";
-            this.Column1.MinimumWidth = 8;
-            this.Column1.Name = "Column1";
-            this.Column1.Width = 150;
-            // 
-            // Column2
-            // 
-            this.Column2.HeaderText = "Column2";
-            this.Column2.MinimumWidth = 8;
-            this.Column2.Name = "Column2";
-            this.Column2.Width = 150;
-            // 
-            // BtnNewPatient
-            // 
-            this.BtnNewPatient.Location = new System.Drawing.Point(165, 144);
-            this.BtnNewPatient.Name = "BtnNewPatient";
-            this.BtnNewPatient.Size = new System.Drawing.Size(148, 33);
-            this.BtnNewPatient.TabIndex = 1;
-            this.BtnNewPatient.Text = "New";
-            this.BtnNewPatient.UseVisualStyleBackColor = true;
-            this.BtnNewPatient.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // BtnEditPatient
-            // 
-            this.BtnEditPatient.Location = new System.Drawing.Point(328, 144);
-            this.BtnEditPatient.Name = "BtnEditPatient";
-            this.BtnEditPatient.Size = new System.Drawing.Size(82, 33);
-            this.BtnEditPatient.TabIndex = 2;
-            this.BtnEditPatient.Text = "Edit";
-            this.BtnEditPatient.UseVisualStyleBackColor = true;
-            // 
-            // BtnDelete
-            // 
-            this.BtnDelete.Location = new System.Drawing.Point(428, 144);
-            this.BtnDelete.Name = "BtnDelete";
-            this.BtnDelete.Size = new System.Drawing.Size(77, 33);
-            this.BtnDelete.TabIndex = 3;
-            this.BtnDelete.Text = "Delete";
-            this.BtnDelete.UseVisualStyleBackColor = true;
-            // 
-            // FormPatient
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1292, 842);
-            this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.BtnClear);
-            this.Controls.Add(this.btnSave);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.dateTimePicker1);
-            this.Controls.Add(this.txtAddress);
+            this.btnSave.Location = new System.Drawing.Point(30, 230);
+           // this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+
+            // btnNew
+            this.btnNew.Text = "New";
+            this.btnNew.Location = new System.Drawing.Point(110, 230);
+           // this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+
+            // btnEdit
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.Location = new System.Drawing.Point(190, 230);
+           // this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+
+            // btnDelete
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.Location = new System.Drawing.Point(270, 230);
+           // this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+
+            // btnClear
+            this.btnClear.Text = "Clear";
+            this.btnClear.Location = new System.Drawing.Point(350, 230);
+            //this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+
+            // dgvPatients
+            this.dgvPatients.Location = new System.Drawing.Point(30, 270);
+            this.dgvPatients.Size = new System.Drawing.Size(500, 150);
+
+            // Form
+            this.ClientSize = new System.Drawing.Size(600, 450);
+            this.Controls.Add(this.lblName);
             this.Controls.Add(this.txtName);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Name = "FormPatient";
-            this.Text = "FormPatient";
-            this.Load += new System.EventHandler(this.FormPatient_Load);
-            this.groupBox1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Controls.Add(this.lblDOB);
+            this.Controls.Add(this.dtpDOB);
+            this.Controls.Add(this.lblGender);
+            this.Controls.Add(this.cmbGender);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.lblPhone);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.dgvPatients);
+            this.Text = "Patient Management";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPatients)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
-
         }
-
-        #endregion
-
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox txtName;
-        private System.Windows.Forms.TextBox txtAddress;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Button btnSave;
-        private System.Windows.Forms.Button BtnClear;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Button BtnDelete;
-        private System.Windows.Forms.Button BtnEditPatient;
-        private System.Windows.Forms.Button BtnNewPatient;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
     }
 }
--- Clinic Management Project/GUI/FormPatient.cs
+++ Clinic Management Project/GUI/FormPatient.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using MySql.Data.MySqlClient;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ClinicManagementSystem;
 
 namespace Clinic_Management_Project.GUI
 {
@@ -15,61 +17,139 @@ namespace Clinic_Management_Project.GUI
         public FormPatient()
         {
             InitializeComponent();
+            LoadPatients();
         }
 
-        private void FormPatient_Load(object sender, EventArgs e)
+        private void LoadPatients()
         {
+            MySqlConnection conn = DatabaseConnection.GetConnection();
 
+            try
+            {
+                conn.Open();
+                string sql = "SELECT id, name, date_of_birth, gender, address, phone FROM patients";
+                MySqlDataAdapter adapter = new MySqlDataAdapter(sql, conn);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                dgvPatients.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
         }
 
-        private void label2_Click(object sender, EventArgs e)
+        private void btnNew_Click(object sender, EventArgs e)
         {
-
+            ClearFields();
         }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private void btnSave_Click(object sender, EventArgs e)
         {
+            MySqlConnection conn = DatabaseConnection.GetConnection();
 
+            try
+            {
+                conn.Open();
+                string sql = "INSERT INTO patients (name, date_of_birth, gender, address, phone) VALUES (@name, @dob, @gender, @address, @phone)";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@name", txtName.Text);
+                cmd.Parameters.AddWithValue("@dob", dtpDOB.Value);
+                cmd.Parameters.AddWithValue("@gender", cmbGender.Text);
+                cmd.Parameters.AddWithValue("@address", txtAddress.Text);
+                cmd.Parameters.AddWithValue("@phone", txtPhone.Text);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Patient added successfully!");
+                LoadPatients();
+                ClearFields();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
         }
 
-        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        private void btnEdit_Click(object sender, EventArgs e)
         {
+            if (dgvPatients.CurrentRow == null) return;
 
+            string id = dgvPatients.CurrentRow.Cells[0].Value.ToString();
+            MySqlConnection conn = DatabaseConnection.GetConnection();
+
+            try
+            {
+                conn.Open();
+                string sql = "UPDATE patients SET name=@name, date_of_birth=@dob, gender=@gender, address=@address, phone=@phone WHERE id=@id";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.Parameters.AddWithValue("@name", txtName.Text);
+                cmd.Parameters.AddWithValue("@dob", dtpDOB.Value);
+                cmd.Parameters.AddWithValue("@gender", cmbGender.Text);
+                cmd.Parameters.AddWithValue("@address", txtAddress.Text);
+                cmd.Parameters.AddWithValue("@phone", txtPhone.Text);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Patient updated successfully!");
+                LoadPatients();
+                ClearFields();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void btnDelete_Click(object sender, EventArgs e)
         {
+            if (dgvPatients.CurrentRow == null) return;
 
+            string id = dgvPatients.CurrentRow.Cells[0].Value.ToString();
+            MySqlConnection conn = DatabaseConnection.GetConnection();
+
+            try
+            {
+                conn.Open();
+                string sql = "DELETE FROM patients WHERE id=@id";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Patient deleted successfully!");
+                LoadPatients();
+                ClearFields();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void btnClear_Click(object sender, EventArgs e)
         {
-
+            ClearFields();
         }
 
-        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        private void ClearFields()
         {
-
-        }
-
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-
+            txtName.Clear();
+            txtAddress.Clear();
+            txtPhone.Clear();
+            cmbGender.SelectedIndex = -1;
+            dtpDOB.Value = DateTime.Today;
         }
     }
 }
