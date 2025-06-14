eb9a35039fa3a18b0033f315ebd2db88a4ee0e8b
zaqyy <arzaqauffer07@gmail.com>
2025-06-15 03:02:16 +0530

    coded and designed prescription form with download function

--- Clinic Management Project/GUI/FormPrescription.Designer.cs
+++ Clinic Management Project/GUI/FormPrescription.Designer.cs
@@ -2,15 +2,17 @@
 {
     partial class FormPrescription
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Label lblAppointment;
+        private System.Windows.Forms.Label lblDetails;
+        private System.Windows.Forms.ComboBox cmbAppointment;
+        private System.Windows.Forms.TextBox txtDetails;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.DataGridView dgvPrescriptions;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,194 +22,127 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.BtnDownloadPrescription = new System.Windows.Forms.Button();
-            this.BtnSavePrescription = new System.Windows.Forms.Button();
-            this.BtnCreatePrescription = new System.Windows.Forms.Button();
-            this.TxtMedication = new System.Windows.Forms.TextBox();
-            this.TxtDiagnosis = new System.Windows.Forms.TextBox();
-            this.LblMedication = new System.Windows.Forms.Label();
-            this.LblDiagnosis = new System.Windows.Forms.Label();
-            this.DtpDateIssued = new System.Windows.Forms.DateTimePicker();
-            this.LblDateIssued = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.DgvPrescriptions = new System.Windows.Forms.DataGridView();
-            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.DgvPrescriptions)).BeginInit();
+            this.lblAppointment = new System.Windows.Forms.Label();
+            this.lblDetails = new System.Windows.Forms.Label();
+            this.cmbAppointment = new System.Windows.Forms.ComboBox();
+            this.txtDetails = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.dgvPrescriptions = new System.Windows.Forms.DataGridView();
+            this.btnDownload = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPrescriptions)).BeginInit();
             this.SuspendLayout();
             // 
-            // groupBox1
+            // lblAppointment
             // 
-            this.groupBox1.Controls.Add(this.BtnDownloadPrescription);
-            this.groupBox1.Controls.Add(this.BtnSavePrescription);
-            this.groupBox1.Controls.Add(this.BtnCreatePrescription);
-            this.groupBox1.Controls.Add(this.TxtMedication);
-            this.groupBox1.Controls.Add(this.TxtDiagnosis);
-            this.groupBox1.Controls.Add(this.LblMedication);
-            this.groupBox1.Controls.Add(this.LblDiagnosis);
-            this.groupBox1.Controls.Add(this.DtpDateIssued);
-            this.groupBox1.Controls.Add(this.LblDateIssued);
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(493, 287);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Prescription Details";
+            this.lblAppointment.Location = new System.Drawing.Point(30, 30);
+            this.lblAppointment.Name = "lblAppointment";
+            this.lblAppointment.Size = new System.Drawing.Size(100, 23);
+            this.lblAppointment.TabIndex = 0;
+            this.lblAppointment.Text = "Appointment";
             // 
-            // BtnDownloadPrescription
+            // lblDetails
             // 
-            this.BtnDownloadPrescription.Location = new System.Drawing.Point(368, 184);
-            this.BtnDownloadPrescription.Name = "BtnDownloadPrescription";
-            this.BtnDownloadPrescription.Size = new System.Drawing.Size(89, 32);
-            this.BtnDownloadPrescription.TabIndex = 8;
-            this.BtnDownloadPrescription.Text = "Download";
-            this.BtnDownloadPrescription.UseVisualStyleBackColor = true;
+            this.lblDetails.Location = new System.Drawing.Point(30, 70);
+            this.lblDetails.Name = "lblDetails";
+            this.lblDetails.Size = new System.Drawing.Size(100, 23);
+            this.lblDetails.TabIndex = 2;
+            this.lblDetails.Text = "Details";
             // 
-            // BtnSavePrescription
+            // cmbAppointment
             // 
-            this.BtnSavePrescription.Location = new System.Drawing.Point(242, 184);
-            this.BtnSavePrescription.Name = "BtnSavePrescription";
-            this.BtnSavePrescription.Size = new System.Drawing.Size(89, 32);
-            this.BtnSavePrescription.TabIndex = 7;
-            this.BtnSavePrescription.Text = "Save";
-            this.BtnSavePrescription.UseVisualStyleBackColor = true;
+            this.cmbAppointment.Location = new System.Drawing.Point(120, 30);
+            this.cmbAppointment.Name = "cmbAppointment";
+            this.cmbAppointment.Size = new System.Drawing.Size(200, 28);
+            this.cmbAppointment.TabIndex = 1;
             // 
-            // BtnCreatePrescription
+            // txtDetails
             // 
-            this.BtnCreatePrescription.Location = new System.Drawing.Point(142, 184);
-            this.BtnCreatePrescription.Name = "BtnCreatePrescription";
-            this.BtnCreatePrescription.Size = new System.Drawing.Size(74, 32);
-            this.BtnCreatePrescription.TabIndex = 6;
-            this.BtnCreatePrescription.Text = "Create";
-            this.BtnCreatePrescription.UseVisualStyleBackColor = true;
+            this.txtDetails.Location = new System.Drawing.Point(120, 70);
+            this.txtDetails.Multiline = true;
+            this.txtDetails.Name = "txtDetails";
+            this.txtDetails.Size = new System.Drawing.Size(300, 60);
+            this.txtDetails.TabIndex = 3;
             // 
-            // TxtMedication
+            // btnSave
             // 
-            this.TxtMedication.Location = new System.Drawing.Point(142, 134);
-            this.TxtMedication.Name = "TxtMedication";
-            this.TxtMedication.Size = new System.Drawing.Size(315, 26);
-            this.TxtMedication.TabIndex = 5;
+            this.btnSave.Location = new System.Drawing.Point(30, 150);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save";
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
-            // TxtDiagnosis
+            // btnEdit
             // 
-            this.TxtDiagnosis.Location = new System.Drawing.Point(142, 83);
-            this.TxtDiagnosis.Name = "TxtDiagnosis";
-            this.TxtDiagnosis.Size = new System.Drawing.Size(315, 26);
-            this.TxtDiagnosis.TabIndex = 4;
+            this.btnEdit.Location = new System.Drawing.Point(110, 150);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 5;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
             // 
-            // LblMedication
+            // btnDelete
             // 
-            this.LblMedication.AutoSize = true;
-            this.LblMedication.Location = new System.Drawing.Point(18, 140);
-            this.LblMedication.Name = "LblMedication";
-            this.LblMedication.Size = new System.Drawing.Size(86, 20);
-            this.LblMedication.TabIndex = 3;
-            this.LblMedication.Text = "Medication";
+            this.btnDelete.Location = new System.Drawing.Point(190, 150);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 6;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
-            // LblDiagnosis
+            // btnClear
             // 
-            this.LblDiagnosis.AutoSize = true;
-            this.LblDiagnosis.Location = new System.Drawing.Point(18, 83);
-            this.LblDiagnosis.Name = "LblDiagnosis";
-            this.LblDiagnosis.Size = new System.Drawing.Size(79, 20);
-            this.LblDiagnosis.TabIndex = 2;
-            this.LblDiagnosis.Text = "Diagnosis";
+            this.btnClear.Location = new System.Drawing.Point(270, 150);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "Clear";
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
             // 
-            // DtpDateIssued
+            // dgvPrescriptions
             // 
-            this.DtpDateIssued.Location = new System.Drawing.Point(142, 38);
-            this.DtpDateIssued.Name = "DtpDateIssued";
-            this.DtpDateIssued.Size = new System.Drawing.Size(315, 26);
-            this.DtpDateIssued.TabIndex = 1;
+            this.dgvPrescriptions.ColumnHeadersHeight = 34;
+            this.dgvPrescriptions.Location = new System.Drawing.Point(30, 190);
+            this.dgvPrescriptions.Name = "dgvPrescriptions";
+            this.dgvPrescriptions.RowHeadersWidth = 62;
+            this.dgvPrescriptions.Size = new System.Drawing.Size(500, 200);
+            this.dgvPrescriptions.TabIndex = 8;
             // 
-            // LblDateIssued
+            // btnDownload
             // 
-            this.LblDateIssued.AutoSize = true;
-            this.LblDateIssued.Location = new System.Drawing.Point(18, 38);
-            this.LblDateIssued.Name = "LblDateIssued";
-            this.LblDateIssued.Size = new System.Drawing.Size(96, 20);
-            this.LblDateIssued.TabIndex = 0;
-            this.LblDateIssued.Text = "Date Issued";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(30, 370);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(108, 20);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "DataGridView";
-            // 
-            // DgvPrescriptions
-            // 
-            this.DgvPrescriptions.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DgvPrescriptions.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Column1,
-            this.Column2});
-            this.DgvPrescriptions.Location = new System.Drawing.Point(12, 416);
-            this.DgvPrescriptions.Name = "DgvPrescriptions";
-            this.DgvPrescriptions.RowHeadersWidth = 62;
-            this.DgvPrescriptions.RowTemplate.Height = 28;
-            this.DgvPrescriptions.Size = new System.Drawing.Size(365, 58);
-            this.DgvPrescriptions.TabIndex = 2;
-            // 
-            // Column1
-            // 
-            this.Column1.HeaderText = "";
-            this.Column1.MinimumWidth = 8;
-            this.Column1.Name = "Column1";
-            this.Column1.Width = 150;
-            // 
-            // Column2
-            // 
-            this.Column2.HeaderText = "";
-            this.Column2.MinimumWidth = 8;
-            this.Column2.Name = "Column2";
-            this.Column2.Width = 150;
+            this.btnDownload.Location = new System.Drawing.Point(350, 150);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(100, 23);
+            this.btnDownload.TabIndex = 0;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
             // 
             // FormPrescription
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1255, 814);
-            this.Controls.Add(this.DgvPrescriptions);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.groupBox1);
+            this.ClientSize = new System.Drawing.Size(600, 420);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.lblAppointment);
+            this.Controls.Add(this.cmbAppointment);
+            this.Controls.Add(this.lblDetails);
+            this.Controls.Add(this.txtDetails);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.dgvPrescriptions);
             this.Name = "FormPrescription";
-            this.Text = "FormPrescription";
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.DgvPrescriptions)).EndInit();
+            this.Text = "Prescription Management";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPrescriptions)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
-        #endregion
-
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Button BtnDownloadPrescription;
-        private System.Windows.Forms.Button BtnSavePrescription;
-        private System.Windows.Forms.Button BtnCreatePrescription;
-        private System.Windows.Forms.TextBox TxtMedication;
-        private System.Windows.Forms.TextBox TxtDiagnosis;
-        private System.Windows.Forms.Label LblMedication;
-        private System.Windows.Forms.Label LblDiagnosis;
-        private System.Windows.Forms.DateTimePicker DtpDateIssued;
-        private System.Windows.Forms.Label LblDateIssued;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.DataGridView DgvPrescriptions;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.Button btnDownload;
     }
 }
--- Clinic Management Project/GUI/FormPrescription.cs
+++ Clinic Management Project/GUI/FormPrescription.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ClinicManagementSystem;
+using MySql.Data.MySqlClient;
 
 namespace Clinic_Management_Project.GUI
 {
@@ -15,6 +17,181 @@ namespace Clinic_Management_Project.GUI
         public FormPrescription()
         {
             InitializeComponent();
+            LoadPrescriptions();
+            LoadAppointments();
+        }
+
+        private void LoadPrescriptions()
+        {
+            MySqlConnection conn = DatabaseConnection.GetConnection();
+            try
+            {
+                conn.Open();
+                string sql = "SELECT p.id, a.id AS appointment_id, p.details FROM prescriptions p JOIN appointments a ON p.appointment_id = a.id";
+                MySqlDataAdapter adapter = new MySqlDataAdapter(sql, conn);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                dgvPrescriptions.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void LoadAppointments()
+        {
+            MySqlConnection conn = DatabaseConnection.GetConnection();
+            try
+            {
+                conn.Open();
+                string sql = "SELECT id FROM appointments";
+                MySqlDataAdapter adapter = new MySqlDataAdapter(sql, conn);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                cmbAppointment.DataSource = dt;
+                cmbAppointment.DisplayMember = "id";
+                cmbAppointment.ValueMember = "id";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            MySqlConnection conn = DatabaseConnection.GetConnection();
+            try
+            {
+                conn.Open();
+                string sql = "INSERT INTO prescriptions (appointment_id, details) VALUES (@appointment_id, @details)";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@appointment_id", cmbAppointment.SelectedValue);
+                cmd.Parameters.AddWithValue("@details", txtDetails.Text);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Prescription added successfully!");
+                LoadPrescriptions();
+                ClearFields();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (dgvPrescriptions.CurrentRow == null) return;
+
+            string id = dgvPrescriptions.CurrentRow.Cells[0].Value.ToString();
+            MySqlConnection conn = DatabaseConnection.GetConnection();
+            try
+            {
+                conn.Open();
+                string sql = "UPDATE prescriptions SET appointment_id=@appointment_id, details=@details WHERE id=@id";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.Parameters.AddWithValue("@appointment_id", cmbAppointment.SelectedValue);
+                cmd.Parameters.AddWithValue("@details", txtDetails.Text);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Prescription updated successfully!");
+                LoadPrescriptions();
+                ClearFields();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (dgvPrescriptions.CurrentRow == null) return;
+
+            string id = dgvPrescriptions.CurrentRow.Cells[0].Value.ToString();
+            MySqlConnection conn = DatabaseConnection.GetConnection();
+            try
+            {
+                conn.Open();
+                string sql = "DELETE FROM prescriptions WHERE id=@id";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Prescription deleted successfully!");
+                LoadPrescriptions();
+                ClearFields();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            if (dgvPrescriptions.CurrentRow == null)
+            {
+                MessageBox.Show("Please select a prescription to download.");
+                return;
+            }
+
+            string id = dgvPrescriptions.CurrentRow.Cells["id"].Value.ToString();
+            string appointmentId = dgvPrescriptions.CurrentRow.Cells["appointment_id"].Value.ToString();
+            string details = dgvPrescriptions.CurrentRow.Cells["details"].Value.ToString();
+
+            SaveFileDialog sfd = new SaveFileDialog
+            {
+                Filter = "Text Files (*.txt)|*.txt",
+                FileName = $"Prescription_{id}.txt"
+            };
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.File.WriteAllText(sfd.FileName,
+                    $"Prescription ID: {id}\nAppointment ID: {appointmentId}\nDetails:\n{details}");
+                MessageBox.Show("Prescription downloaded successfully!");
+            }
+        }
+
+
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearFields();
+        }
+
+        private void ClearFields()
+        {
+            cmbAppointment.SelectedIndex = -1;
+            txtDetails.Clear();
+            if (dgvPrescriptions.CurrentRow != null)
+            {
+                dgvPrescriptions.ClearSelection();
+            }
         }
     }
 }
+
+    
+     
