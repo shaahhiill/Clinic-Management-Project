26ed998739ad29eb9972a7e7fc1c6652b60a1712
zaqyy <arzaqauffer07@gmail.com>
2025-06-14 20:56:35 +0530

    cleared FormAppointment errors

--- Clinic Management Project/UI/FormAppointment.cs
+++ Clinic Management Project/UI/FormAppointment.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Clinic_Management_Project.UI
 {
-    internal class FormAppointment
+    public partial class FormAppointment : Form
     {
     }
 }
