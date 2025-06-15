6a9e1a36cedfef65562dfba6840ed0a46a4ac4f2
zaqyy <arzaqauffer07@gmail.com>
2025-06-15 12:16:12 +0530

    created, coded and designed notification form

---
+++ Clinic Management Project/GUI/FormNotification.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Clinic_Management_Project.GUI
+{
+    partial class FormNotification
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Label lblRecipient;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.TextBox txtRecipient;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.DataGridView dgvNotifications;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.lblRecipient = new System.Windows.Forms.Label();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.txtRecipient = new System.Windows.Forms.TextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.dgvNotifications = new System.Windows.Forms.DataGridView();
+
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNotifications)).BeginInit();
+            this.SuspendLayout();
+
+            this.lblRecipient.Text = "Recipient";
+            this.lblRecipient.Location = new System.Drawing.Point(30, 30);
+            this.txtRecipient.Location = new System.Drawing.Point(120, 30);
+            this.txtRecipient.Size = new System.Drawing.Size(200, 23);
+
+            this.lblMessage.Text = "Message";
+            this.lblMessage.Location = new System.Drawing.Point(30, 70);
+            this.txtMessage.Location = new System.Drawing.Point(120, 70);
+            this.txtMessage.Size = new System.Drawing.Size(300, 60);
+            this.txtMessage.Multiline = true;
+
+            this.btnSend.Text = "Send";
+            this.btnSend.Location = new System.Drawing.Point(30, 150);
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+
+            this.btnClear.Text = "Clear";
+            this.btnClear.Location = new System.Drawing.Point(110, 150);
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+
+            this.dgvNotifications.Location = new System.Drawing.Point(30, 190);
+            this.dgvNotifications.Size = new System.Drawing.Size(500, 200);
+
+            this.ClientSize = new System.Drawing.Size(600, 420);
+            this.Controls.Add(this.lblRecipient);
+            this.Controls.Add(this.txtRecipient);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.dgvNotifications);
+            this.Text = "Notification Management";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNotifications)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ Clinic Management Project/GUI/FormNotification.cs
@@ -0,0 +1,87 @@
+﻿using ClinicManagementSystem;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clinic_Management_Project.GUI
+{
+    public partial class FormNotification : Form
+    {
+        public FormNotification()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtRecipient.Text) || string.IsNullOrWhiteSpace(txtMessage.Text))
+            {
+                MessageBox.Show("Please enter recipient and message.");
+                return;
+            }
+
+            MySqlConnection conn = DatabaseConnection.GetConnection();
+            try
+            {
+                conn.Open();
+                string sql = "INSERT INTO notifications (recipient, message, sendDate) VALUES (@recipient, @message, @sendDate)";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@recipient", txtRecipient.Text);
+                cmd.Parameters.AddWithValue("@message", txtMessage.Text);
+                cmd.Parameters.AddWithValue("@sendDate", DateTime.Now);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Notification sent and logged successfully!");
+                LoadNotifications();
+                ClearFields();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearFields();
+        }
+
+        private void ClearFields()
+        {
+            txtRecipient.Clear();
+            txtMessage.Clear();
+        }
+
+        private void LoadNotifications()
+        {
+            MySqlConnection conn = DatabaseConnection.GetConnection();
+            try
+            {
+                conn.Open();
+                string sql = "SELECT id, recipient, message, sendDate FROM notifications";
+                MySqlDataAdapter adapter = new MySqlDataAdapter(sql, conn);
+                var dt = new System.Data.DataTable();
+                adapter.Fill(dt);
+                dgvNotifications.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading notifications: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+}
